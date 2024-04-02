74e26033edace7eaa63aa408a11559aacf601dbf
unfairdude <easynafta@gmail.com>
2024-04-02 19:05:08 +0200

    eerer

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,6 @@
 const SITE_TITLE = '$STICKMAN'
 
-const BRIEF_DESCRIPTION = 'The father of all memes.\nWe are coming.'
+const BRIEF_DESCRIPTION = 'The father of all memes. \n We are coming.'
 
 const FULL_DESCRIPTION =
 	'Compare GitHub changelogs across multiple releases in a single view'
